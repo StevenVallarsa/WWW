cbbb8c9282572e0f18f9001b5aa83c0621033268
Brad <bjkapteyn@gmail.com>
2019-06-17 21:58:35 -0400

    Added Google maps on homepag and added a marker when you enter address

---
+++ WeatherWorryWonder/Content/GoogleMaps.js
@@ -0,0 +1,8 @@
+﻿
+    var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 42.969887, lng: -85.664538},
+          zoom: 10
+        });
+      }
