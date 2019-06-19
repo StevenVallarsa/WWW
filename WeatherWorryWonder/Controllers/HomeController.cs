3257b2a2412145626f10d8c8b47a6d5ea673253e
Steven <orionsjv@gmail.com>
2019-06-19 15:53:36 -0400

    Working on output for AQIs in Color

--- WeatherWorryWonder/Controllers/HomeController.cs
+++ WeatherWorryWonder/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WeatherWorryWonder.Controllers
     public class HomeController : Controller
     {
         WeatherWorryWonderDBEntities db = new WeatherWorryWonderDBEntities();
+
         public ActionResult Index()
         {
             JToken weather = WeatherAPIDAL.Json();
@@ -65,52 +66,54 @@ namespace WeatherWorryWonder.Controllers
             rv.O3AQI = AQIForO3;
             rv.PredictedAQITomorrow = FutureAQIForO3;
 
+            Recommendations(AQIForO3);
+            ViewBag.AQI = AQIForO3;
+
             return View(rv);
         }
 
 
 
 
-        public ActionResult Recommendations(decimal reading)
+        public void Recommendations(decimal reading)
         {
-            if (reading >= 0 || reading <= 50)
+            if (reading >= 0 && reading <= 50)
             {
                 //hex green
                 ViewBag.AQIColor = "#42f445";
                 ViewBag.Suggestions = "It’s a great day to be active outside.";
 
             }
-            else if (reading >= 51 || reading <= 100)
+            else if (reading >= 51 && reading <= 100)
             {
                 //hex yellow
                 ViewBag.AQIColor = "#f4f142";
                 ViewBag.Suggestions = "Unusually sensitive people: Consider reducing prolonged or heavy outdoor exertion. Watch for symptoms such as coughing or shortness of breath. These are signs to take it a little easier.";
             }
-            else if (reading >= 101 || reading <= 150)
+            else if (reading >= 101 && reading <= 150)
             {
                 //hex orange
                 ViewBag.AQIColor = "#f49241";
                 ViewBag.Suggestions = "Sensitive groups: Reduce prolonged or heavy outdoor exertion. Take more breaks, do less intense activities. Watch for symptoms such as coughing or shortness of breath. Schedule outdoor activities in the morning when ozone is lower. People with asthma should follow their asthma action plans and keep quick relief medicine handy.";
             }
-            else if (reading >= 151 || reading <= 200)
+            else if (reading >= 151 && reading <= 200)
             {
                 //hex red
                 ViewBag.AQIColor = "#42f445";
                 ViewBag.Suggestions = "Sensitive groups: Avoid prolonged or heavy outdoor exertion. Schedule outdoor activities in the morning when ozone is lower. Consider moving activities indoors. People with asthma, keep quick-relief medicine handy. Everyone else: Reduce prolonged or heavy outdoor exertion. Take more breaks, do less intense activities.Schedule outdoor activities in the morning when ozone is lower.";
             }
-            else if (reading >= 201 || reading <= 300)
+            else if (reading >= 201 && reading <= 300)
             {
                 //hex purple
                 ViewBag.AQIColor = "#a100f1";
                 ViewBag.Suggestions = "Sensitive groups: Avoid all physical activity outdoors. Move activities indoors or reschedule to a time when air quality is better. People with asthma, keep quick-relief medicine handy. Everyone else: Avoid prolonged or heavy outdoor exertion. Schedule outdoor activities in the morning when ozone is lower.Consider moving activities indoors.";
             }
-            else if (reading >= 301 || reading <= 500)
+            else if (reading >= 301 && reading <= 500)
             {
                 //hex maroon
                 ViewBag.AQIColor = "#800000";
                 ViewBag.Suggestions = "	Everyone: Avoid all physical activity outdoors";
             }
-            return View();
 
         }
 
