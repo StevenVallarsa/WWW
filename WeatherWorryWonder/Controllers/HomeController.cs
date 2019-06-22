5994a6e8dea07a759acaa720f99e528ce5dd2324
Callista <theevenstarlight@gmail.com>
2019-06-21 21:45:07 -0400

    Added OzoneReductionTips form in the view

--- WeatherWorryWonder/Controllers/HomeController.cs
+++ WeatherWorryWonder/Controllers/HomeController.cs
@@ -183,33 +183,6 @@ namespace WeatherWorryWonder.Controllers
             return View(sensors);
         }
 
-        //public ActionResult CallistasRecommendationsSoYouBetterListen()
-        //{
-        //    < form action = "myaction" >
-
-
-        //     < select name = "param" >
-
-
-        //     < option value = "volvo" > Volvo </ option >
-
-
-        //      < option value = "saab" > Saab </ option >
-
-
-        //      < option value = "opel" > Opel </ option >
-
-
-        //       < option value = "audi" > Audi </ option >
-
-
-        //       </ select >
-
-
-        //       < input type = "submit" />
-
-
-        //        </ form >
 
         public ActionResult OzoneReductionTips(string option)   //Callista
         {
